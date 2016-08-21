8e4b206232884deddfc6351116c47921eddedad2
vrde <agranzot@gmail.com>
2016-08-21 02:42:31 +0200

    Add adaptor to parse HTML

--- userscript/src/scrape.js
+++ userscript/src/scrape.js
@@ -1,13 +1,19 @@
-import cheerio from 'cheerio';
+// import cheerio from 'cheerio';
+
+import $ from 'jquery';
 
 
 export function scrape(post) {
 }
 
 
-export function identify(post) {
-    var $ = cheerio.load(post);
-    if ($('.uiStreamSponsoredLink').length == 1) {
+export function identify(post, parser) {
+    if (typeof parser === 'undefined') {
+        parser = $;
+    }
+
+    var elem = parser(post);
+    if (elem.find('.uiStreamSponsoredLink').length == 1) {
         return 'sponsored';
     }
 }
--- userscript/test/utils.js
+++ userscript/test/utils.js
@@ -1,5 +1,10 @@
 import fs from 'fs';
+import cheerio from 'cheerio'
 
+
+export function parser(html) {
+    return { find: cheerio.load(html) };
+}
 
 export function loadFixture(name) {
     return fs.readFileSync('./test/fixtures/' + name + '.html').toString();
