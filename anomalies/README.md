a365391b45cf2de49844c13ea98f245ba7d8d73e
vecna <vecna@globaleaks.org>
2016-08-25 12:04:53 +0200

    progress in the anomalies tester and fixed a refresh/Id/Unique mistake

--- anomalies/README.md
+++ anomalies/README.md
@@ -1,15 +1,28 @@
 
-# anomalies managmement
+# Anomalies Managmement
 
 from ESCVI root:
 
     mkdir samples
+
+daily duty: (still todo a cleaner from /dev/shm)
+
     anomalies/importerror.sh
 
-check maybe the errors present in /dev/shm/\*.json
+then run the (not yet interactive)
 
-    anomaly=1 anomalies/resender.js
+    anomaly=1 mongodb="mongodb://localhost/biny" npm run watch
 
-loop again with the script moving from /dev/shm to ./samples
+note: anomaly ENV variable permit to save the anomalies happening, if
+is set to 0 don't save. it is set in config/settings.json
+
+    DEBUG=* anomalies/resender.js
+
+when you spot an error in the console of ESCVI, you press ^c, copy paste the last file as --file=filename.json in the argument, for example:
+
+    delay=1500 DEBUG=* anomalies/resender.js --file=parsingError-104444444444444-080049.json
+
+and iterated with your debug.
+To loop again with the new saved errors, this script move files from /dev/shm to ./samples
 
     anomalies/looperror.sh
--- anomalies/resender.js
+++ anomalies/resender.js
@@ -1,32 +1,47 @@
 #!/usr/bin/env nodejs
 var _ = require('lodash');
 var Promise = require('bluebird');
+var util = require('util');
 var fs = Promise.promisifyAll(require('fs'));
 var request = Promise.promisifyAll(require('request'));
 var debug = require('debug')('reSender');
 var nconf = require('nconf')
 
-
 var url ='http://localhost:8000';
 // var url ='https://facebook.tracking.exposed';
 
-nconf.argv()
-     .env();
+nconf.argv().env();
 
-if (_.isNaN(_.parseInt(nconf.get('delay')))) {
-    console.log("env variable 'delay' is required");
+if ( _.isUndefined(nconf.get('DEBUG')) || 
+     _.isNaN(_.parseInt(nconf.get('delay'))) ) {
+    console.log("Set 'DEBUG' env variable and 'delay' ");
     return -1;
 }
 
+var delayTime = _.parseInt(nconf.get('delay'));
+
+var files = fs
+    .readdirAsync('samples')
+    .then(function(files) {
+        debugger;
+        if(_.isUndefined(nconf.get('file'))) {
+          // TODO hook the --unit and send only that 
+            return files;
+        } else {
+            debug("Using the single file as configured");
+            delayTime = 0;
+            return [ nconf.get('file') ];
+        }
+    });
+
 debug("Using url %s", url);
-return fs
-  .readdirAsync('samples')
-  .reduce(function(memo, fname, i, total) {
-      debug("Processing file\t%d\t%d\t%s", i, total, fname);
+return Promise.reduce(files, function(memo, fname, i, total) {
+      debug("Processing \t%d/%d\t%s", i, total, fname);
       var fpath = 'samples/' + fname;
       return fs.readFileAsync(fpath, "utf-8")
         .then(JSON.parse)
         .then(function(filecontent) {
+          debug("firing error from %s", filecontent.user.href);
           return request({
             url: url + '/F/2',
             method: "POST",
@@ -34,14 +49,13 @@ return fs
             json: true
           });
         })
+        .delay(delayTime)
         .then(function(result) {
-            debug("File %s result: %j", fpath, result);
             memo[fpath] = true;
             return memo;
         })
-        .delay(_.parseInt(nconf.get('delay')))
         .catch(function(error) {
-            debug("File %s ERROR!: %j", fpath, error);
+            debug("ERROR!: %j", error);
             memo[fpath] = false;
             return memo;
         });
