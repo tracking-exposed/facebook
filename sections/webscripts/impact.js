8c92189517b3b46bb20ea5c13a54c351bf545c90
vecna <vecna@globaleaks.org>
2016-09-12 13:09:51 +0200

    new /impact page skeleton

---
+++ sections/webscripts/impact.js
@@ -0,0 +1,11 @@
+
+var displayTimeAdoption = function(containerId) {
+    var url = '/public/view/2/';
+    d3.json(url, function(something) {
+
+      console.log(something);
+      // graph X/Y to be done
+
+    });
+};
+
