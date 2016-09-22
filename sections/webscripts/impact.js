de833a391eb45d3569cf0f286f7c29ab01669c28
vecna <vecna@globaleaks.org>
2016-09-22 19:15:32 +0200

    extended with daily users activity the impact log

--- sections/webscripts/impact.js
+++ sections/webscripts/impact.js
@@ -1,15 +1,36 @@
 var displayTimeAdoption = function(containerId) {
     var url = '/node/activity/2/column';
-
     d3.json(url, function(something) {
-
-        console.log(something);
-
         var chart = c3.generate({
             bindto: containerId,
             data: {
                 x: 'x',
-                columns: something
+                columns: something,
+                names: {
+                    timeline: "Number of posts recorded",
+                    refreshes: "Number of refreshes",
+                    supporters: "Users' last day of recorded activity",
+                    users: "Number of active user"
+                },
+                axes: { 
+                    timeline: 'y',
+                    refreshes: 'y',
+                    supporters: 'y2', 
+                    users: 'y2' 
+                },
+                types: {
+                    timeline: 'line',
+                    refreshes: 'area',
+                    supporters: 'bar',
+                    users: 'spline'
+                },
+                labels: true,
+                colors: {
+                    timeline: '#afa3ae',
+                    refreshes: '#8b9dc3',
+                    supporters: '#3b5998',
+                    users: '#3b5998'
+                }
             },
             axis: {
                 x: {
@@ -17,11 +38,9 @@ var displayTimeAdoption = function(containerId) {
                     tick: {
                         format: '%Y-%m-%d'
                     }
-                }
+                },
+                y2: { show: true }
             }
         });
-
     });
-
 };
-
