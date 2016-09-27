a5c90b74a05c5c3e5110e35d31b695b3377b6c5f
vrde <agranzot@gmail.com>
2016-09-27 23:54:55 +0200

    Add button, refactor parsing

--- userscript/src/app.js
+++ userscript/src/app.js
@@ -1,40 +1,43 @@
 // Install any necessary polyfills into global, such as es6, stage/3, stage/4, etc. as needed
-/*
-import 'core-js/es6';
+//
+require('../styles/app.scss');
 
+import 'core-js/es6';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Replace as necessary with your own root app
-const JsReactorApp = () => (
-    <h1>JS Reactor App</h1>
-);
-
-// Replace as necessary with your root app and DOM element to inject into
-ReactDOM.render((<JsReactorApp />), document.getElementById('js-reactor-app'));
-*/
-
-require('../styles/app.scss');
-
+import $ from 'jquery';
 import 'arrive';
 import { identify } from './scrape';
+
+import StartButton from './components/startButton';
 
 function boot () {
     console.log('ESCVI loading!');
     prefeed();
     watch();
+    render();
 };
 
 function prefeed () {
     document.querySelectorAll('.userContentWrapper').forEach(function (elem) {
-        console.log('new element', identify(elem), elem);
+        console.log('new element', identify($(elem)), elem);
     });
 };
 
 function watch () {
     document.arrive('.userContentWrapper', function () {
-        console.log('new element', identify(this), this);
+        console.log('new element', identify($(this)), this);
     });
+};
+
+function render () {
+    const rootElement = $('<div />', { 'id': 'escvi--root' });
+
+    $('body').append(rootElement);
+
+    // Replace as necessary with your root app and DOM element to inject into
+    ReactDOM.render((<StartButton />), document.getElementById('escvi--root'));
 };
 
 boot();
--- userscript/src/scrape.js
+++ userscript/src/scrape.js
@@ -1,19 +1,13 @@
-// import cheerio from 'cheerio';
-
-import $ from 'jquery';
-
 export function scrape (post) {
 }
 
-export function identify (post, parser) {
-    if (typeof parser === 'undefined') {
-        parser = $;
-    }
-
-    var elem = parser(post);
+export function identify (elem) {
     if (elem.find('.uiStreamSponsoredLink').length === 1) {
         return 'sponsored';
     } else {
         return 'post';
     }
+}
+
+export function scrapeBasicInfo () {
 }
