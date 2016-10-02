6e04d8f8122f4993a7f4dddfbfde4f16482a1f25
vrde <agranzot@gmail.com>
2016-10-03 01:44:53 +0200

    Add util to get current time with time zone

---
+++ userscript/src/utils.js
@@ -0,0 +1,18 @@
+export function currentTimeISO8601 (date) {
+    // Thanks to http://stackoverflow.com/a/17415677/597097
+    const now = date || new Date();
+    const tzo = -now.getTimezoneOffset();
+    const dif = tzo >= 0 ? '+' : '-';
+    const pad = function (num) {
+        const norm = Math.abs(Math.floor(num));
+        return (norm < 10 ? '0' : '') + norm;
+    };
+    return [now.getFullYear(), '-',
+            pad(now.getMonth() + 1), '-',
+            pad(now.getDate()), 'T',
+            pad(now.getHours()), ':',
+            pad(now.getMinutes()), ':',
+            pad(now.getSeconds()), dif,
+            pad(tzo / 60), ':',
+            pad(tzo % 60)].join('');
+}
