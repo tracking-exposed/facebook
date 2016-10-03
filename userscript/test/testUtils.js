3691eb3fa676a672bc1ae0bceaafe704f77632c0
vrde <agranzot@gmail.com>
2016-10-03 19:20:52 +0200

    Add TimeWarp to mock time

--- userscript/test/testUtils.js
+++ userscript/test/testUtils.js
@@ -1,12 +1,23 @@
 import { assert } from 'chai';
+import { TimeWarp } from './utils';
 import { getTimeISO8601 } from '../src/utils';
 
 describe('utils.getTimeISO8601', function () {
-    it('returns the current time', function () {
+    const timeWarp = new TimeWarp();
+
+    afterEach(() => {
+        timeWarp.reset();
+    });
+
+    it('returns the current time if no args', function () {
+        timeWarp.set(2001, 11, 10, 6, 30, 30, -240);
+        assert.equal(getTimeISO8601(), '2001-12-10T06:30:30+04:00');
+    });
+
+    it('converts a Date instance to ISO8601', function () {
         const date = new Date(2000, 6, 1, 15, 0, 0, 0);
-        date.getTimezoneOffset = function () {
-            return -240;
-        };
-        assert.equal(getTimeISO8601(date), '2000-07-01T15:00:00+04:00');
+        date.getTimezoneOffset = () => -60;
+
+        assert.equal(getTimeISO8601(date), '2000-07-01T15:00:00+01:00');
     });
 });
--- userscript/test/utils.js
+++ userscript/test/utils.js
@@ -8,3 +8,25 @@ export function $ (html) {
 export function loadFixture (name) {
     return $(fs.readFileSync('./test/fixtures/' + name + '.html').toString());
 }
+
+export class TimeWarp {
+    set () {
+        const args = Array.prototype.slice.call(arguments, 0, 6);
+        const offset = arguments[6] || 0;
+
+        if (!this._Date) {
+            this._Date = global.Date;
+        }
+
+        global.Date = () => {
+            // Thanks to http://stackoverflow.com/a/8843181/597097
+            const date = new (Function.prototype.bind.call(this._Date, null, ...args));
+            date.getTimezoneOffset = () => offset;
+            return date;
+        };
+    }
+
+    reset () {
+        global.Date = this._Date;
+    }
+}
