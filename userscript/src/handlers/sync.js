bcd3549dd5067708dd4eff117993e19611786ea0
vrde <agranzot@gmail.com>
2016-10-13 18:26:57 +0200

    wip on sync

--- userscript/src/handlers/sync.js
+++ userscript/src/handlers/sync.js
@@ -1,15 +1,39 @@
-const INTERVAL = 1000;
-const posts = [];
-var lastCheck = null;
+const INTERVAL = 50000;
 
-function eventHandler (type, e) {
-    posts.push(e);
+var currentTimeline = null;
+var timelines = [];
+
+function handlePost (type, e) {
+    e.data.position = currentTimeline.lastPosition++;
+    currentTimeline.posts.push(e.data);
+}
+
+function handleTimeline (type, e) {
+    currentTimeline = {
+        uuid: e.uuid,
+        dt: e.dt,
+        location: window.location.href,
+        lastPosition: 1,
+        posts: []
+    };
+
+    timelines.push(currentTimeline);
 }
 
 function sync () {
+    // send timelines
+    console.debug('sync', timelines.filter((timeline) => timeline.posts.length));
 
+    // remove all other timelines
+    timelines = timelines.slice(timelines.length - 1);
+
+    // empty the "posts" array in currentTimeline
+    currentTimeline.posts = [];
 }
 
 export function register (hub) {
-    hub.register('newPost', eventHandler);
+    hub.register('newPost', handlePost);
+    hub.register('newTimeline', handleTimeline);
+    hub.register('windowUnload', sync);
+    window.setInterval(sync, INTERVAL);
 }
