857f0917596774e78fadcb40951d3e0649328c9b
vecna <vecna@globaleaks.org>
2016-10-15 04:14:52 +0200

    basic realitymeter visualisation

--- sections/webscripts/realitymeter.js
+++ sections/webscripts/realitymeter.js
@@ -1,19 +1,20 @@
 
 var displayRealityGraph = function(postId, containerId) {
-    console.log("displayRealityGraph of postId " + postId);
     if(!postId || postId === "0") return;
     var url = '/post/reality/2/' + postId;
 
     var margin = {top: 20, right: 20, bottom: 30, left: 60},
         width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 500 - margin.top - margin.bottom,
+        /* the space in the 'x' line before the 1st tick & after the last */
+        padding = 40;
 
     /* more the post is in a favorable position, and darker should be */
     var color = d3.scale.linear()
-        .domain([30, 0])
-        .range(['green', 'steelblue']);
+        .domain([1, 30])
+        .range(['yellow', 'darkgreen']);
 
-    var x = d3.scale.ordinal().rangePoints([0, width]);
+    var x = d3.scale.ordinal().rangePoints([padding, width - (padding * 2)]);
     var y = d3.time.scale().range([height, 0]);
 
     var xAxis = d3.svg.axis().scale(x).orient("bottom");
@@ -28,6 +29,11 @@ var displayRealityGraph = function(postId, containerId) {
     var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ").parse;
 
     d3.json(url, function(data) {
+
+        var stats = _.countBy(data, function(d) {
+            return d.presence ? d.userId + "Y" : d.userId + "N";
+        }, []);
+        var firstViz = new Date(_.last(_.sortBy(data, 'refreshTime')).refreshTime);
 
         x.domain(_.map(data, function(d) {
             return d.userId;
@@ -48,7 +54,7 @@ var displayRealityGraph = function(postId, containerId) {
             .attr("x", width)
             .attr("y", -6)
             .style("text-anchor", "end")
-            .text("Users");
+            .text("User ID");
 
         svg.append("g")
             .attr("class", "y axis")
@@ -59,7 +65,7 @@ var displayRealityGraph = function(postId, containerId) {
             .attr("y", 6)
             .attr("dy", ".71em")
             .style("text-anchor", "end")
-            .text("Published on: xxx");
+            .text("Display after " + firstViz.toLocaleString() );
 
         svg.selectAll(".dot")
             .data(_.filter(data, function(d) { return d.presence; }))
@@ -72,8 +78,7 @@ var displayRealityGraph = function(postId, containerId) {
             })
             .attr("cy", function(d) { return y(d.refreshTime); })
             .style("stroke", function(d) {
-                // return color(d.order);
-                return 'yellow';
+                return color(d.order + 10);
             })
             .style("fill", function(d) {
                 return color(d.order);
@@ -97,39 +102,46 @@ var displayRealityGraph = function(postId, containerId) {
             });
 
 
-        console.log(color.domain());
-
         var legend = svg.selectAll(".legend")
-            .data([30, 20, 10, 1])
+            .data([
+                { 'value': 1,
+                  'visibility': 'max' },
+                { 'value': 10,
+                  'visibility': 'high' },
+                { 'value': 20,
+                  'visibility': 'medium' },
+                { 'value': 30,
+                  'visibility': 'low' }
+            ])
             .enter().append("g")
             .attr("class", "legend")
             .attr("transform", function(d, i) {
                 return "translate(0," + i * 20 + ")";
             });
-
         legend.append("rect")
             .attr("x", width - 18)
             .attr("width", 18)
             .attr("height", 18)
-            .style("fill", color);
-
+            .style("fill", function(d) {
+                return color(d.value);
+            });
         legend.append("text")
             .attr("x", width - 24)
             .attr("y", 9)
             .attr("dy", ".35em")
             .style("text-anchor", "end")
-            .text(function(d) { return "Post positoning in feed: " + d; });
+            .text(function(d) {
+                return d.visibility;
+            });
     });
 };
 
 /* not working at the moment the 'click' trapping of button ? */
 var getPostId = function(formId) {
-    var writtenv = document.getElementById(formId);
-    console.log(writtenv);
-    var postId = parseInt("3323");
+    var writtenv = document.getElementById(formId).value;
+    var postId = _.parseInt(writenv);
 
     if(postId === NaN)
         return null;
-    console.log("parsed " + postId);
     return postId;
 };
