cfb1626cf2d690c6f1228ac32b6571fc105ae59d
vecna <vecna@globaleaks.org>
2016-10-15 04:32:05 +0200

    fix express4 error code, killed topPosts

--- operations/tryAPI.js
+++ operations/tryAPI.js
@@ -58,7 +58,6 @@ var testByUser = function(alli) {
 var testByPost= function(alli) {
     var aPost = getInfo(alli, 'postId');
     return Promise.all([
-        apiR(url, ['post', 'top', version ]),
         apiR(url, ['post', 'reality', version, aPost])
     ]);
 };
@@ -66,14 +65,16 @@ var testByPost= function(alli) {
 var testByUserPost = function(alli) {
     var aPost = getInfo(alli, 'postId');
     var anUser = getInfo(alli, 'userId');
-    return apiR(url, ['post', 'perceived', version, aPost, anUser]);
+    return Promise.all([
+        apiR(url, ['post', 'perceived', version, aPost, anUser])
+    ]);
 };
 
 var testNode = function(alli) {
     return Promise.all([
           apiR(url, ['node', 'activity', version, kind]),
           apiR(url, ['node', 'countries', version, kind]),
-          apiR(url, ['node', 'country', version, 'IT', kind]),
+          apiR(url, ['node', 'country', version, 'IT', kind])
     ]);
 };
 
