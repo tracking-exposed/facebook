28fa4a5106ee1d10ecb0bf21b71a78554366aafb
vecna <vecna@globaleaks.org>
2016-10-27 23:01:31 +0200

    implemented basic fixtures for event v1

--- operations/v1fixtures.js
+++ operations/v1fixtures.js
@@ -5,7 +5,8 @@ var util = require('util');
 var fs = Promise.promisifyAll(require('fs'));
 var request = Promise.promisifyAll(require('request'));
 var debug = require('debug')('tryAPI');
-var nconf = require('nconf')
+var moment = require('moment');
+var nconf = require('nconf');
 
 nconf.argv().env();
 
@@ -24,19 +25,36 @@ var pickedO = null;
 /* this is the utilty for all the connection */
 var apiR = function(base, api) {
     var URL = base + api;
+    var timelineId = "XXXX-blah-3432424-" + _.random(1, 0xffff);
     debug("POST ⇒ %s", URL);
     return request
-        .postAsync(URL, {form: {type: 'artist'}})
+        .postAsync(URL, {form: [
+          {
+            "type": "timeline",
+            "id": timelineId,
+            "startTime": moment().toISOString()
+          },
+          {
+            "type": "post",
+            "visibility": "public",
+            "impressionTime": moment().add(1, 'year').toISOString(),
+            "impressionOrder": 1,
+            "timelineId": timelineId,
+            "html": "<html>snippet</html>"
+          },
+          {
+            "type": "post",
+            "visibility": "private",
+            "impressionTime": moment().add(11, 'months').toISOString(),
+            "impressionOrder": 2,
+            "timelineId": timelineId
+          }
+        ]})
         .then(function(response) {
             return JSON.parse(response.body);
         })
         .tap(function(infos) {
-            debug("Retrieved %s", URL);
-            var toPrint = nconf.get('print');
-            _.each(api, function(de) {
-                if(de === toPrint)
-                    console.log(JSON.stringify(infos, undefined, 2));
-            });
+            console.log(JSON.stringify(infos, undefined, 2));
         })
         .catch(function(error) {
             debug("!Error with %s: %s", URL, error);
