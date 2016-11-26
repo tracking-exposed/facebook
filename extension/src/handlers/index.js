ec80f7815db472f1511dd151a21573fb2451e8ed
vecna <claudio@tracking.exposed>
2016-11-26 00:26:35 -0200

    this is the simplest way ATM to removed the visualDebugBox, right @vrde ?

--- extension/src/handlers/index.js
+++ extension/src/handlers/index.js
@@ -1,5 +1,5 @@
 export function registerHandlers (hub) {
     require('./sync').register(hub);
     require('./logger').register(hub);
-    require('./visualDebug').register(hub);
+    // require('./visualDebug').register(hub);
 }
