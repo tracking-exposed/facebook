af1e1df08ac9d723d3640ffa4f4957b1bb852c77
claudio → vecna <claudio@tracking.exposed>
2017-03-14 17:38:35 -0300

    implemented nation wide dumper for research, prototype

--- mongo-scripts/national-csv.js
+++ mongo-scripts/national-csv.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env nodejs
 var _ = require('lodash');
 var Promise = require('bluebird');
-var debug = require('debug')('hourly-usage');
+var fs = Promise.promisifyAll(require('fs'));
+var debug = require('debug')('national-csv');
 var moment = require('moment');
 var nconf = require('nconf');
 
@@ -28,41 +29,127 @@ var timeFilter = timutils.doFilter(
     );
 var sourceNation = nconf.get('nation');
 
+if(!sourceNation)
+    throw new Error("`sourceNation` as environment variable is required");
+
+var destFile = sourceNation + "-" + moment(timeFilter.start).format("YYYY-MM-DD") +'+30d.csv';
+
+var keys = [
+    'id',
+    'userPseudo',
+    'publicationUTime',
+    'impressionTime',
+    'impressionOrder',
+    'postId',
+    'permaLink',
+    'timelineId',
+    'counter'
+];
+
 debug("Executing timewindow: %s and nation %s",
     timutils.prettify(timeFilter), sourceNation);
 
-
-    var filter = { startTime: {
-        "$gt": new Date(timeFilter.start),
-        "$lt": new Date(timeFilter.end)
-    } };
-
-function getImpressions() {
-    var filter = { impressionTime: {
-        "$gt": new Date(timeFilter.start),
-        "$lt": new Date(timeFilter.end)
-    } };
+function getFromHtmls(timeline) {
     return mongo
-      .countByMatch(nconf.get('schema').impressions, filter);
+        .read(nconf.get('schema').htmls, { timelineId: timeline.id, type: 'feed' })
+        .reduce(function(memo, e) {
+            if(!e || !e.id)
+                return memo;
+
+            var metadata = _.pick(e, ['id', 'publicationUTime', 'postId', 'permaLink', 'timelineId' ]);
+            memo[e.id] = metadata;
+            return memo;
+        }, {});
+};
+
+function getFromImpressions(timeline) {
+    return mongo
+        .read(nconf.get('schema').impressions, { timelineId: timeline.id })
+        .reduce(function(memo, imp) {
+            if(!imp || !imp.htmlId)
+                return memo;
+
+            memo[imp.htmlId] = {
+                userId: imp.userId,
+                impressionOrder: imp.impressionOrder,
+                impressionTime: moment(imp.impressionTime).toISOString(),
+            };
+            return memo;
+        }, {});
+};
+
+function appendCSV(timeline, counter) {
+
+    return Promise.all([
+            getFromHtmls(timeline),
+            getFromImpressions(timeline)
+    ])
+    .then(function(combos) {
+        return _.map(combos[0], function(metadata, id) {
+            if(!combos[1][id])
+                debugger;
+            var fix = _.merge(
+                    _.pick(metadata, ['permaLink', 'hrefType', 'postId', 'publicationUTime' ]),
+                    combos[1][id]
+                );
+            fix.userPseudo = utils.numId2Words([1, fix.userId]);
+            fix.counter = counter;
+            return fix;
+        });
+    })
+    .then(function(content) {
+        var csvContent = _.reduce(content, function(memo, entry) {
+            /* fill memo as side effect, use declared keys above */
+
+            _.each(keys, function(k, i) {
+                var swap;
+                if(k === 'publicationUTime') {
+                    swap = _.get(entry, k);
+                    swap = moment(swap * 1000).toISOString();
+                } else {
+                    swap = _.get(entry, k, "");
+                    swap = _.replace(swap, /\"/g, '〃');
+                }
+                memo +=  '"' + swap + '"';
+                if(!_.eq(i, _.size(keys) - 1))
+                    memo += ',';
+            });
+            memo += "\n";
+            return memo;
+
+        }, "");
+        if(_.size(csvContent)) {
+            debug("appending %d entries first %j", _.size(content), content[0]);
+            return fs.appendFileAsync(destFile, csvContent, 'utf8');
+        } else {
+            debug("not appending");
+        }
+    })
+    .catch(function(error) {
+        debug("Error: %s", error);
+        return false;
+    })
+    .return(true);
+
 }
 
-function appendCSV(timeline) {
-    return mongo.read(
-}
+function startCSV(timelines) {
+    debug("Operating over %d timelines, init %s", _.size(timelines), destFile);
+    return fs
+        .writeFileAsync(destFile, keys.join(",") + "\n")
+};
 
-
-function getTimelineByCountry(tlc) {
-
-    debug("Having as TLC %s", tlc);
+function getTimelinesByCountry(tlc) {
 
     var filter = { startTime: {
         "$gt": new Date(timeFilter.start),
         "$lt": new Date(timeFilter.end)
     }, geoip: tlc };
+    debug("Selecting timelines as: %s", JSON.stringify(filter, undefined, 2));
     return mongo
         .read(nconf.get('schema').timelines, filter, {startTime: -1})
 };
 
 return getTimelinesByCountry(sourceNation)
   .tap(startCSV)
-  .map(appendCSV, { concurrency: 20 });
+  .map(appendCSV, { concurrency: _.parseInt(nconf.get('concurrency')) || 1 });
