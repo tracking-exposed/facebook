57c1e991b911e453d19fa136b5b68071688830f5
Edoardo Tenani <edoardo.tenani@gmail.com>
2017-03-15 23:58:42 +0100

    refactor imageAltTag parser implementation

--- parsers/imageAltTag.js
+++ parsers/imageAltTag.js
@@ -9,32 +9,24 @@ var parse = require('./lib/parse');
  */
 
 var PARSER_COMPLETED_SUCCESSFULLY = false;
-var FACEBOOK_ALT_TEXT_START = 'Image may contain:';
-
-function parseAltContent(altText) {
-    var content = altText.split(FACEBOOK_ALT_TEXT_START)[1];
-    var parsedWords = content.split(',');
-    parsedWords = _.map(parsedWords, function (e) { return e.trim() });
-    return parsedWords;
-}
 
 function implementation(snippet) {
 
     debug("→ %j", _.omit(snippet, ['html']));
 
     var $ = cheerio.load(snippet.html);
-    var returnValue = [];
 
-    var altTagFounds = $('img[alt^="'+FACEBOOK_ALT_TEXT_START+'"]');
-
-    _.each(altTagFounds, function (e) {
-        returnValue.push(parseAltContent(e.attribs.alt));
-    })
-    returnValue = _.flatten(returnValue);
+    var altTagFounds = $('img[alt]');
+    altTagFounds = _.map(altTagFounds, function (e) {
+      return e.attribs.alt;
+    });
+    altTagFounds = _.flatten(altTagFounds);
+    altTagFounds = _.uniq(altTagFounds);
 
     PARSER_COMPLETED_SUCCESSFULLY = true;
     return {
-        'imageAltTag': returnValue
+        imageAltTag: PARSER_COMPLETED_SUCCESSFULLY,
+        altTexts: altTagFounds
     }
 };
 
