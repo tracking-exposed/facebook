ba56b275ed473dfc1c6c878454ea2e21dcf1cc21
vecna <claudio@tracking.exposed>
2017-06-12 21:55:18 +0200

    completed imageAltText

--- parsers/imageAltTag.js
+++ parsers/imageAltTag.js
@@ -5,41 +5,49 @@ var moment = require('moment');
 var debug = require('debug')('parser:imageAltText');
 var parse = require('./lib/parse');
 
-/*
- * Extract alt text from img tags
- */
-
 function imageAltText(snippet) {
 
     var $ = cheerio.load(snippet.html);
-
     var altTagFounds = $('img[alt]');
+
     altTagFounds = _.map(altTagFounds, function (e) {
-      return e.attribs.alt;
+        try {
+            if(e.parent.attribs['data-hovercard'])
+                return null;
+        } catch(c) { };
+        try {
+            if(e.parent.attribs.role)
+                return null;
+        } catch(c) { };
+
+        return e.attribs.alt;
     });
-    debug("Found %j", altTagFounds);
+
     altTagFounds = _.flatten(altTagFounds);
     altTagFounds = _.reject(altTagFounds, "");
+    altTagFounds = _.compact(altTagFounds);
     altTagFounds = _.uniq(altTagFounds);
 
-    debug("Found %j", altTagFounds);
-
     if(!_.size(altTagFounds)) {
-        debug("Failed in %s", snippet.id);
+        debug("Not found altText in %s", snippet.id);
         return { imageAltText: false };
     }
-    else {
-        debug("Found %j", altTagFounds);
-        return {
-            imageAltText: true,
-            altTexts: altTagFounds
-        };
-    }
+
+    if(_.size(altTagFounds) === 1)
+        var altTexts = _.first(altTagFounds);
+    else
+        var altTexts = _.join(altTagFounds, ' ⁘ ');
+
+    debug("Found %s [%s]: %s", snippet.id, snippet.hrefType, altTexts);
+    return {
+        imageAltText: true,
+        altTexts: altTexts
+    };
 };
 
 return parse.please({
     'name': 'imageAltText',
-    'requirements': { hrefType: 'photo' },
+    'requirements': { type: 'feed' },
     'implementation': imageAltText,
     'since': "2017-12-09",
     'until': moment().toISOString(),
