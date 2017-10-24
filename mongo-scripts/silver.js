9447bd1bf374aa3f5c9e2acb91ef0d03e3e6abe1
Claudio Agosti <claudio@tracking.exposed>
2017-10-24 19:20:18 +0200

    final fixes before data extraction

--- mongo-scripts/silver.js
+++ mongo-scripts/silver.js
@@ -50,68 +50,101 @@ debug("Executing timewindow: %s %s timeline filter %s, htmls filter %s +feed onl
     JSON.stringify(timef, undefined, 2),
     JSON.stringify(htmlf, undefined, 2) );
 
+var patterns = {
+    /* the regexp is case insensitive and global, because we want a count */
+    "Nisman": new RegExp("Nisman", 'ig'),
+    "Maldonado": new RegExp("Maldonado", 'ig'),
+    "Kirchner": new RegExp("Kirchner", 'ig'),
+    "Bullrich": new RegExp("Bullrich", 'ig'),
+    "Massa": new RegExp("Massa", 'ig')
+};
+
+function patternMap(htmlstring) {
+
+    return _.reduce(patterns, function(memo, re, pattern) {
+        var matches = htmlstring.match(re);
+        var l = matches ? matches.length : 0;
+        _.set(memo, pattern, l);
+        return memo;
+    }, {});
+};
 
 function lookintoHTMLs(timeline, counter) {
 
     return Promise.all([
         mongo.read(nconf.get('schema').htmls, {
             timelineId: timeline.id,
-            type: 'feed'
-        }),
+            publicationUTime: { "$exists": true }
+        }, { savingTime: -1 }),
         mongo.read(nconf.get('schema').impressions, {
             timelineId: timeline.id
         }),
         timeline.name
     ])
     .then(function(combos) {
-
+/*
         debug("htmls %d, impressions %d, %s timeline %s of %s",
             _.size(combos[0]), _.size(combos[1]), combos[2],
-            timeline.startTime, timeline.name);
+            timeline.id, timeline.name);
+*/
+        if(_.size(combos[0]) < 3)
+            return [];
 
-        return _.map(combos[1], function(impression, i) {
+        return _.map(combos[0], function(html, i) {
 
-            var html = _.find(combos[0], { id: impression.htmlId });
-
-            if(!html) {
-                impression.name = combos[2]
-                return _.omit(impression, ['_id', 'id', 'htmlId' ])
-            }
+            var impression = _.find(combos[1], { htmlId: html.id });
+            var ret = new Object();
 
             if(html.permaLink) {
                 if(html.hrefType === "groupPost")
-                    html.sourceId = html.permaLink.split('/')[2];
+                    ret.pageName = html.permaLink.split('/')[2];
                 else
-                    html.sourceId = html.permaLink.split('/')[1];
+                    ret.pageName = html.permaLink.split('/')[1];
             }
 
-            var ret = impression;
+            ret.profile = combos[2];
 
-            ret.name = combos[2];
-            ret.geoip = timeline.geoip;
-            ret.publicationTime = moment(html.publicationUTime * 1000);
+            if(!html.postId) {
+                debug("Warning, no postId in %s", html.id);
+                return null;
+            }
 
-            return _.merge(
-                _.omit(ret, ['_id', 'id', 'htmlId' ]),
-                _.omit(html, ['_id', 'html', 'impressionId', 'postType',
-                              'publicationUTime', 'feedUTime', 'type',
-                          'feedText', 'feedHref', 'feedBasicInfo',
-                          'imageAltText', 'savingTime' ])
+            ret.postId = String(html.postId);
+
+            if(html.externalHref) {
+                ret.externals = _.map(html.externalHref, function(link) {
+                    return {
+                        link: link,
+                        id: various.hash({
+                            'href': link,
+                            'type': "original"
+                        })
+                    };
+                });
+            }
+
+            /* the timings are recorded in GTM, I want to display in Buenos Aires time */
+            ret.impressionTime = moment(impression.impressionTime).utcOffset(-180).format();
+            ret.publicationTime = moment(html.publicationUTime * 1000).utcOffset(-180).format();
+            ret.visualizationDiff = moment
+                .duration(
+                    moment(impression.impressionTime).utcOffset(-180) -
+                    moment(html.publicationUTime * 1000).utcOffset(-180)
+                ).asSeconds();
+
+            ret.reactions = html.rmap;
+            ret.interactions = {
+                total: html.rtotal,
+                comments: html.comments,
+                shares: html.shares
+            };
+            ret.displayName = html.source;
+
+            return _.merge(ret, patternMap(html.html),
+                _.pick(impression, ['impressionOrder' ]),
+                _.pick(html, ['id', 'hrefType', 'timelineId' ])
             );
         });
-    })
-    .then(function(content) {
-
-        var str = _.reduce(content, function(memo, elem) {
-            if(memo !== "")
-                memo += ",\n";
-
-            memo += JSON.stringify(elem, undefined, 2);
-            return memo;
-        }, "");
-
-        if(str !== "")
-            return appendPromise(destFile, str + ",\n");
     });
 }
 
@@ -135,7 +168,6 @@ function appendPromise(fpath, str, reset=false) {
 
 function beginQuery(user) {
 
-    debug("User %j", user);
     var filter = _.extend(timef, {
         startTime: {
             "$gt": new Date(timeFilter.start),
@@ -151,20 +183,28 @@ function beginQuery(user) {
             e.name = user.name;
             return e;
         })
-        .map(lookintoHTMLs, { concurrency: 1 });
+        .tap(function(r) {
+            debug("¹ user %j contributed with %d timelines", user, _.size(r));
+        })
+        .map(lookintoHTMLs, { concurrency: 1 })
+        .then(_.flatten)
+        .tap(function(r) {
+            debug("² user %j contributed with %d posts", user, _.size(r));
+        });
 };
 
 return various
     .loadJSONfile("config/users.json")
-    .tap(function(users) {
-        debug("Creating file");
-        return appendPromise(destFile, "[\n", true);
-    })
     .then(function(c) {
         return c['silver'];
     })
     .map(beginQuery, { concurrency: 1})
-    .tap(function() {
-        return appendPromise(destFile, "]");
-        debug("Complete! remind to delete the last comma of: %s", destFile);
+    .then(_.flatten)
+    .tap(function(c) {
+        debug("All the posts are %d", _.size(c));
+        /* saving the file, all the data are kept togheder */
+        return appendPromise(destFile, JSON.stringify(c, undefined, 2), reset=true);
+    })
+    .tap(function(c) {
+        debug("Done! %s saved", destFile);
     });
