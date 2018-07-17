77631bedba31a5ca52f4e06eba2c9e0740850339
Claudio <claudio@tracking.exposed>
2018-07-17 18:26:10 +0200

    updated the promotedTitle parser, since 2018-07-17 18:00 circa, promotedName is a new field

--- alltheparser.sh
+++ alltheparser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DEBUG=* url="http://localhost:8000" parsers/postType.js
 DEBUG=*,-parser:core url="http://localhost:8000" parsers/promotedTitle.js 2>&1 >&/tmp/promoted.log
-DEBUG=*,-parser:core url="http://localhost:8000" parsers/promotedLink.js 2>&1 >&/tmp/promoted.log
+DEBUG=*,-parser:core url="http://localhost:8000" parsers/promotedLink.js
 DEBUG=*,-parser:core url="http://localhost:8000" parsers/promotedInfo.js
 DEBUG=*,-parser:core url="http://localhost:8000" parsers/feedUTime.js
 DEBUG=*,-parser:core url="http://localhost:8000" parsers/feedBasicInfo.js
--- parsers/promotedTitle.js
+++ parsers/promotedTitle.js
@@ -15,55 +15,54 @@ nconf.set("PARSER_PROMOTEDTITLE_VERSION", "201612.03");
 
 function getPromotedTitle(snippet) {
 
-	var title;
-    var name;
-	var title_type;
-	var found = false;
-	var e_threshold;
-	var e_ptr;
-		
-	postcount ++;
-	
+    var title;
+    var name = null;
+    var title_type;
+    var found = false;
+    var e_threshold;
+    var e_ptr;
+
+    postcount ++;
+
     var $ = cheerio.load(snippet.html);
     
-	var e_threshold = $('div.userContent');
-	
-	e_ptr = $(e_threshold).find("p").first();
-	if (e_ptr.text() !== "") {
-		title = e_ptr.text();
-		found = true;
-	} else {
-		e_ptr = $(e_threshold).next().find(".clearfix").first();
-		e_ptr.children("div").each(function() {
-			if (!$(this).find("button")[0]) {
-				if ($(this).children().first().text() === entities.decodeHTML($(this).children().first().html()) && $(this).children().first().text() !== "") {
-					title = $(this).children().first().text();
-					found = true;
-				}
-			}
-		});
-	}
+    var e_threshold = $('div.userContent');
+
+    e_ptr = $(e_threshold).find("p").first();
+    if (e_ptr.text() !== "") {
+        title = e_ptr.text();
+        found = true;
+    } else {
+        e_ptr = $(e_threshold).next().find(".clearfix").first();
+        e_ptr.children("div").each(function() {
+            if (!$(this).find("button")[0]) {
+                if ($(this).children().first().text() === entities.decodeHTML($(this).children().first().html()) && $(this).children().first().text() !== "") {
+                    title = $(this).children().first().text();
+                    found = true;
+                }
+            }
+        });
+    }
 
     try {
         var name = $('a[data-hovercard^="/ajax/hovercard/page"]').text();
     } catch(error) {
-        name = null;
+        debug("Unable to extract `name`");
     }
 
-
-	if (!found) {
-		errorcount++;
+    if (!found) {
+        errorcount++;
         debug("Err %d post %d [%s] Title NOT FOUND", errorcount, postcount, snippet.id);
         return { "promotedTitle": false };
-	} else {
-		debug("+[%s] %s ≬ %s (err %d/%d)", snippet.id, name, title, errorcount, postcount);
-	    return { 
+    } else {
+        debug("+[%s] %s ≬ %s (err %d/%d)", snippet.id, name, title, errorcount, postcount);
+        return {
             "promotedTitle": true,
             "title": title,
             "promotedName": name,
             "titleId": utils.hash({'title': title}) 
         };
-	}
+    }
 };
 
 return parse.please({
@@ -73,4 +72,3 @@ return parse.please({
     'since': "2016-09-13",
     'until': moment().toISOString(),
 });
-
