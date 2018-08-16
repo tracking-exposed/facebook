d6615e28cac4132168721998217de4c58516e02a
Claudio <claudio@tracking.exposed>
2018-08-17 00:15:31 +0200

    work in progress on research interface

--- sections/webscripts/research.js
+++ sections/webscripts/research.js
@@ -13,3 +13,51 @@ function distinct(activityGraphId, activeListId) {
         console.log(something);
     });
 };
+
+function specialInit() {
+
+    console.log(window.location);
+    console.log(window.location.pathname);
+
+
+
+    $("#shoot").on('click', function(e) {
+        console.log("click happen");
+    });
+};
+
+
+function postObject() {
+
+    var subjects = [
+      'Government',
+      'Parliamento',
+      'Local Issue',
+      'General theme',
+      'M5S',
+      'Media Debate',
+      'Value Statement',
+      'Personal Consideration'
+    ];
+};
+
+function goal1() {
+
+    var goals = [
+        'Oversight (o surveillance)',
+        'Prevention (o interdiction)',
+        'Judgement (o evaluation)'
+    ];
+};
+
+function goal2() {
+    var goals = [
+        'Responsiveness statement',
+        'Accountability Statement',
+        'Engagment',
+        'Constituency services & advocacy',
+        'Outreach initiatives',
+        'Personal',
+        'Other'
+    ];
+};
