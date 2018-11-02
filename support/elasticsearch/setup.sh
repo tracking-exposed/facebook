e4bea5da2780ccc5b6f59b03e05a28d9065baa04
zoidberg <zoiberg@uranus.com>
2018-11-02 16:04:18 +0000

    added support

---
+++ support/elasticsearch/setup.sh
@@ -0,0 +1,53 @@
+curl -X PUT "localhost:9200/fbtrex" -H 'Content-Type: application/json' -d'
+{
+  "mappings" : {
+    "doc" : {
+      "dynamic": false,
+      "properties": {
+        "type":    { "type": "keyword"  },
+    "feedText":{
+     "dynamic": true,
+     "properties":{}
+    },
+    "feedBasicInfo": {
+     "dynamic": true,
+     "properties":{}
+    },
+    "feedHref": {
+     "dynamic": true,
+     "properties":{}
+    },
+    "feedUTime":{
+     "dynamic": true,
+     "properties":{}
+    },
+    "promotedLink":{
+     "dynamic": true,
+     "properties":{}
+    },
+    "promotedTitle":{
+     "dynamic": true,
+     "properties":{}
+    },
+    "promotedInfo":{
+     "dynamic": true,
+     "properties":{}
+    },
+    "imageAltText": {
+      "dynamic": true,
+      "properties":{
+      }
+    },
+        "savingTime": {
+      "properties": {
+        "$gt": {"type": "date"},
+        "$lt": {"type":  "date"}
+      }
+        },
+    "date": { "type": "date"},
+    "n": {"type": "integer"}
+      }
+      }
+  }
+}
+'
