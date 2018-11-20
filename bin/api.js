bb44a4cebc24360adc29714ad4da1a3fc7a65ee7
Claudio Agosti <claudio@tracking.exposed>
2018-11-20 09:22:37 +0100

    non-working commit of 'static' and 'api' services

--- bin/api.js
+++ bin/api.js
@@ -6,26 +6,25 @@ var moment = require('moment');
 var bodyParser = require('body-parser');
 var Promise = require('bluebird');
 var mongodb = Promise.promisifyAll(require('mongodb'));
-var debug = require('debug')('fbtrex');
+var debug = require('debug')('fbtrex:api');
 var nconf = require('nconf');
 var pug = require('pug');
 var cors = require('cors');
 
-var utils = require('./lib/utils');
-var escviAPI = require('./lib/allversions');
-var performa = require('./lib/performa');
-var mongo = require('./lib/mongo');
+var utils = require('../lib/utils');
+var escviAPI = require('../lib/allversions');
+var performa = require('../lib/performa');
+var mongo = require('../lib/mongo');
 
 var cfgFile = "config/settings.json";
 var redOn = "\033[31m";
 var redOff = "\033[0m";
 
-var echoes = require("./lib/echoes")
+var echoes = require("../lib/echoes")
+echoes.addEcho("elasticsearch")
+echoes.setDefaultEcho("elasticsearch")
 
-nconf.argv()
-     .env()
-     .file({ file: cfgFile })
-     .file('users', { file: "config/users.json" });
+nconf.argv().env().file({ file: cfgFile });
 
 console.log(redOn + "ઉ nconf loaded, using " + cfgFile + redOff);
 
@@ -39,8 +38,6 @@ var returnHTTPError = function(req, res, funcName, where) {
     return false;
 };
 
-echoes.addEcho("elasticsearch")
-echoes.setDefaultEcho("elasticsearch")
 
 /* This function wraps all the API call, checking the verionNumber
  * managing error in 4XX/5XX messages and making all these asyncronous
@@ -50,9 +47,12 @@ function dispatchPromise(name, req, res) {
 
     var apiV = _.parseInt(_.get(req.params, 'version'));
 
+    if(apiV != 2)
+        debug("Migration error: the API are implemented as /api/v2");
+
     /* force version to the only supported version */
-    if(_.isNaN(apiV) || (apiV).constructor !== Number || apiV != 1)
-        apiV = 1;
+    if(_.isNaN(apiV) || (apiV).constructor !== Number || apiV != 2)
+        apiV = 2;
 
     if(_.isUndefined(req.randomUnicode)) {
         req.randomUnicode = inc;
@@ -110,8 +110,8 @@ server.listen(nconf.get('port'), nconf.get('interface'));
 console.log(" Listening on http://" + nconf.get('interface') + ":" + nconf.get('port'));
 /* configuration of express4 */
 app.use(cors());
-app.use(bodyParser.json({limit: '4mb'}));
-app.use(bodyParser.urlencoded({limit: '4mb', extended: true}));
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({limit: '1mb', extended: true}));
 
 app.get('/api/v:version/node/info', function(req, res) {
     return dispatchPromise('nodeInfo', req, res);
@@ -143,26 +143,6 @@ app.post('/api/v:version/query', function(req, res) {
     return dispatchPromise('queryContent', req, res);
 });
 
-/* Parser API */
-app.post('/api/v:version/snippet/status', function(req, res) {
-    return dispatchPromise('snippetAvailable', req, res);
-});
-app.post('/api/v:version/snippet/content', function(req, res) {
-    return dispatchPromise('snippetContent', req, res);
-});
-app.post('/api/v:version/snippet/result', function(req, res) {
-    return dispatchPromise('snippetResult', req, res);
-});
-
-
-/* This is import and validate the key */
-app.post('/api/v:version/validate', function(req, res) {
-    return dispatchPromise('validateKey', req, res);
-});
-/* This to actually post the event collection */
-app.post('/api/v:version/events', function(req, res) {
-    return dispatchPromise('processEvents', req, res);
-});
 
 
 /* HTML single snippet */
@@ -207,14 +187,6 @@ app.get('/impact', function(req, res) {
     return dispatchPromise('getImpact', req, res);
 });
 
-/* via POST the user gived publicKey and fbtrex returns selector + access token */
-app.post('/api/v1/userInfo', function(req, res) {
-    return dispatchPromise('userInfo', req, res);
-});
-/* selector experiment, should be discontinued */
-app.get('/api/v1/selector', function(req, res) {
-    return dispatchPromise('getSelector', req, res);
-});
 
 /* researcher API interfaces */
 app.get('/api/v1/distinct/:authkey', function(req, res) {
@@ -255,79 +227,8 @@ app.get('/api/v1/reducer/:reducerId/:authkey/:start/:end', function(req, res) {
         return dispatchPromise('reducer' + rid, req, res);
 });
 
-/* static files, independent by the API versioning */
-app.get('/favicon.ico', function(req, res) {
-    res.sendFile(__dirname + '/dist/favicon.ico');
-});
-app.get('/robots.txt', function(req, res) {
-    res.sendFile(__dirname + '/dist/robots.txt');
-});
-
 /* RSS endpoint to glue legacy and new system */
 app.get('/api/v1/exportText/:key/:seconds', function(req, res) {
     return dispatchPromise('exportText', req, res);
 });
 
-
-/* development: the local JS are pick w/out "npm run build" every time, and
- * our locally developed scripts stay in /js/local */
-if(nconf.get('development') === 'true') {
-    console.log(redOn + "ઉ DEVELOPMENT = serving JS from src" + redOff);
-    app.use('/js/local', express.static(__dirname + '/sections/webscripts'));
-} else {
-    app.use('/js/local', express.static(__dirname + '/dist/js/local'));
-}
-
-/* catch the other 'vendor' script in /js */
-app.use('/js', express.static(__dirname + '/dist/js'));
-app.use('/css', express.static(__dirname + '/dist/css'));
-app.use('/images', express.static(__dirname + '/dist/images'));
-app.use('/fonts', express.static(__dirname + '/dist/fonts'));
-app.use('/autoscroll.user.js', express.static(__dirname + '/I/autoscroll.user.js'));
-
-/* legacy for realitycheck */
-app.get('/realitycheck/:userId?/:detail?', function(req, res) {
-    req.params.page = 'realitycheck';
-    return dispatchPromise('getPage', req, res);
-});
-
-/* this if someone click on 'Your Data' before opt-in */
-app.get('/personal/unset/:stuff', function(req, res) {
-    req.params.page = 'unset';
-    return dispatchPromise('getPage', req, res);
-});
-
-/* special pages: the parameters are acquired by JS client side */
-app.get('/personal/:userId/:detail', function(req, res) {
-    req.params.page = 'personal';
-    return dispatchPromise('getPage', req, res);
-});
-app.get('/revision/:htmlId', function(req, res) {
-    req.params.page = 'revision';
-    return dispatchPromise('getPage', req, res);
-});
-
-/* last one, page name catch-all */
-app.get('/:page*', function(req, res) {
-    return dispatchPromise('getPage', req, res);
-});
-/* true last */
-app.get('/', function(req, res) {
-    return dispatchPromise('getPage', req, res);
-});
-
-
-function infiniteLoop() {
-    /* this will launch other scheduled tasks too */
-    return Promise
-        .resolve()
-        .delay(60 * 1000)
-        .then(function() {
-            if(_.size(performa.queue))
-                return mongo
-                    .cacheFlush(performa.queue, "performa")
-        })
-        .then(infiniteLoop);
-};
-
-infiniteLoop();
