e92744aca0108a6cca14f5c87834197124a34d98
Claudio Agosti <claudio@tracking.exposed>
2018-12-11 16:33:52 +0100

    new parsing approach implemented

--- parsers/components/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    post: require('./post'),
-    promoted: require('./promoted'),
-    text: require('./text'),
-};
--- parsers/precise.js
+++ parsers/precise.js
@@ -14,18 +14,41 @@ if(!targetId) {
     return;
 }
 
-return mongo
-    .readOne(nconf.get('schema').htmls, { id: targetId })
-    .then(function(html) {
-        return mongo
-            .readOne(nconf.get('schema').impressions, { id: html.impressionId })
-            .then(function(impression) {
-                _.unset(impression, 'id');
-                return _.merge(html, impression);
-            });
-    })
-    .then(parse.impression)
-    .tap(parse.save)
-    .then(function(result) {
-        debug("%s", JSON.stringify(result, undefined, 2));
-    });
+nconf.argv().env().file({ file: 'config/collector.json' });
+return precise(targetId);
+
+function precise(targetId) {
+
+    debug("addressing %s", targetId);
+    return mongo
+        .readOne(nconf.get('schema').htmls, { id: targetId })
+        .then(function(html) {
+            if(!html || !html.impressionId)
+                return [];
+
+            return mongo
+                .readOne(nconf.get('schema').impressions, { id: html.impressionId })
+                .then(function(impression) {
+                    _.unset(impression, 'id');
+                    _.unset(impression, 'htmlId');
+                    return _.merge(html, impression);
+                });
+        })
+        .then(function(single) {
+            if(single)
+                return [ single ];
+        })
+        .map(parse.impression)
+        .tap(parse.mark)
+        .tap(parse.save)
+        .then(function(result) {
+            if(_.size(result))
+                debug("%s", JSON.stringify(result, undefined, 2));
+            else
+                debug("No metadata produced?");
+        });
+}
+
+module.exports = {
+    precise: precise
+};
