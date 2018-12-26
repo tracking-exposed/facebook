869428e1afefc5681ce61085cde7fda8ebaab8eb
Claudio <claudio@tracking.exposed>
2018-12-27 00:57:56 +0100

    used wolfie.js for Janu-May 2018

--- mongo-scripts/wolfie.js
+++ mongo-scripts/wolfie.js
@@ -2,18 +2,17 @@
 result = db.htmls2.aggregate( [ 
     { $match: { 
         'savingTime': { 
-            "$gte": new Date("2018-12-11"),
-            "$lte": new Date("2018-12-14")
+            "$gte": new Date("2018-05-01"),
+            "$lte": new Date("2018-05-31")
         }
     }}, 
     { $project: { 
         timelineId: 1, 
         savingTime: 1, 
-        promoted: { $cond: [{ $eq: [ "$type", "promoted"]}, true, false] },
-        video: { $cond: [{ $eq: [ "$hrefType", "video"]}, true, false] } 
+        promoted: { $cond: [{ $eq: [ "$type", "promoted"]}, true, false] }
     }},
     { $match: { 
-        video: true
+        promoted: true
     }}, { 
       $group: { _id: '$timelineId', total: { $sum: 1}  } 
     } ]
@@ -25,7 +24,7 @@ while ( result.hasNext() ) {
     videos = thing.total;
     amountOf = db.impressions2.count({ timelineId: timelineId });
     percent = Math.round ( ( 100 / amountOf ) * videos);
-    round = Math.round( percent / 5 ) * 5; 
+    round = Math.round( percent / 3 ) * 3; 
 
     if(amountOf > 5) 
         print(round);
