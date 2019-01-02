dc4271ecd13f1b46738a42d37ab348a6ee4b3441
Claudio Agosti <claudio@tracking.exposed>
2019-01-02 12:56:45 +0100

    simple message logger tester for #98

---
+++ test/sendLogMsg.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+var debug = require('debug')('tests:sendLogMsg');
+var nconf = require('nconf');
+
+var echoes = require('../lib/echoes');
+
+const id = Math.round((new Date()).getTime() / 1000);
+const fieldValue = nconf.get('field') || "random-value-" + _.random(0, 0xffff);
+
+
+function sendMessage() {
+    debug("Sending message with `id` %s and `field` %s", id, fieldValue);
+    var r = echoes.echo({
+        id: id,
+        index: "tester_sender",
+        field: fieldValue
+    });
+    return r;
+};
+
+debug("The configuration of elastic is %j", nconf.get('elastic'));
+return sendMessage();
