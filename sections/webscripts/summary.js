2e301d3c5891fb7a184916ebde45b56255f5287c
Claudio <claudio@tracking.exposed>
2019-01-09 02:35:54 +0100

    switched landing to the new (incomplete) summary

--- sections/webscripts/summary.js
+++ sections/webscripts/summary.js
@@ -1,3 +1,6 @@
 function initializeSummary() {
-    console.log("initializeSummary");
+    const token = _.find(window.location.pathname.split('/'), function(e) {
+        return _.size(e) == 40;
+    });
+    $('#summary').text(`${window.location.origin}/api/v1/summary/${token}`);
 };
