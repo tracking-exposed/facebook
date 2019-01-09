ee7637497f40525f36a5b950ce00f7051496d404
Claudio <claudio@tracking.exposed>
2019-01-09 02:49:03 +0100

    fixed summary landing

--- sections/webscripts/summary.js
+++ sections/webscripts/summary.js
@@ -2,5 +2,6 @@ function initializeSummary() {
     const token = _.find(window.location.pathname.split('/'), function(e) {
         return _.size(e) == 40;
     });
-    $('#summary').text(`${window.location.origin}/api/v1/summary/${token}`);
+    const url = `${window.location.origin}/api/v1/summary/${token}`;
+    $('#summary').html(`<a href="${url}">${url}</a>`);
 };
