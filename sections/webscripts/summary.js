01ddd2766d551644bb8327b0279eba72de0fa670
Groschenroman <roman.hanig@posteo.de>
2019-01-22 17:02:05 +0100

    Further layouting

--- sections/webscripts/summary.js
+++ sections/webscripts/summary.js
@@ -6,10 +6,32 @@ function initializeSummary() {
     // $('#summary').html(`<a href="${url}">${url}</a>`);
     $.getJSON(url, function(data) {
       console.log(data);
-      
       for (let i = 0; i < 20; i++) {
-        const gridItem = `<div class="col-sm-6 col-md-4 col-lg-2">${data[i].author}</div>`;
+        const item = data[i];
+
+        // Don't display entries that have errors
+        if (item.errors.length) {
+          continue;
+        }
+
+        const gridItem = `
+          <article class="col-sm-6 col-md-4 col-lg-2 grid-item ${item.type || ''}">
+            <header>
+              <span class="small ${item.postId ? 'post-id' : ''}">${item.postId || '#'}</span>
+            </header>
+            <section class="body">
+              <p><b>${item.author}</b></p>
+              ${item.permaLink
+                ? '<a href="https://facebook.com' + item.permaLink + '" title="Go to post" target="_blank">'+ item.permaLink +'</a>'
+                : ''}
+            </section>
+          </article>
+        `;
         $('#summary').append(gridItem);
       }
+    });
+    $('.grid').isotope({
+      // options
+      itemSelector: '.grid-item'
     });
 };
