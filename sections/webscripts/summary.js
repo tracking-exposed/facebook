657cce401975ad7770d2fd1e88426b7c04ee0340
Groschenroman <roman.hanig@posteo.de>
2019-01-22 15:11:15 +0100

    Start layouting summary (errornous)

--- sections/webscripts/summary.js
+++ sections/webscripts/summary.js
@@ -3,8 +3,13 @@ function initializeSummary() {
         return _.size(e) == 40;
     });
     const url = `${window.location.origin}/api/v1/summary/${token}`;
-    $('#summary').html(`<a href="${url}">${url}</a>`);
+    // $('#summary').html(`<a href="${url}">${url}</a>`);
     $.getJSON(url, function(data) {
       console.log(data);
+      
+      for (let i = 0; i < 20; i++) {
+        const gridItem = `<div class="col-sm-6 col-md-4 col-lg-2">${data[i].author}</div>`;
+        $('#summary').append(gridItem);
+      }
     });
 };
