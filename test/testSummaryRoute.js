2d8cf0a0cd9407b3214410d85b7147d352fb306c
Claudio TREX <claudio@tracking.exposed>
2019-02-27 08:33:45 +0100

    UT small progress syncing

---
+++ test/testSummaryRoute.js
@@ -0,0 +1,118 @@
+const expect    = require("chai").expect;
+const rss = require("../lib/rss");
+
+const fixtures = require('../lib/fixtures');
+
+/* This first check the capacity of load data and verify they are avail */
+describe("Route for personal access: loading data", function() {
+
+  function testExistence(columnName) {
+    describe("testing loaded " + columnName, function() {
+    let hardcoded = _.get(fixtures, ${columnName}N )
+
+      return mongo
+        .count(_.get(nconf.get('schema'), columnName),
+          { userId: fixtures.staticUserId })
+        .then(function(amount) {
+            expect(amount).to.equal(hardcoded);
+        });
+  };
+
+  return fixtures
+    .loadFixtures()
+    .tap(testExistence('timelines'))
+    .tap(testExistence('impressions'))
+    .tap(testExistence('metadata'))
+    .tap(testExistence('htmls'));
+
+});
+
+describe("Test 'page' route", function() {
+});
+describe("Test 'data' route", function() {
+});
+describe("Test 'csv' route", function() {
+});
+describe("Test 'extended' route", function() {
+});
+describe("Test 'semantics' route", function() {
+});
+
+function testBuildRSServ() {
+
+  describe("getFreshlySubscribed", function() {
+      it("retrieve data", function() {
+
+
+      });
+
+  });
+
+};
+
+
+describe("Color Code Converter", function() {
+describe("RGB to Hex conversion", function() {
+  it("converts the basic colors", function() {
+    var redHex   = converter.rgbToHex(255, 0, 0);
+    var greenHex = converter.rgbToHex(0, 255, 0);
+    var blueHex  = converter.rgbToHex(0, 0, 255);
+
+    expect(redHex).to.equal("ff0000");
+    expect(greenHex).to.equal("00ff00");
+    expect(blueHex).to.equal("0000ff");
+  });
+});
+
+describe("Hex to RGB conversion", function() {
+  it("converts the basic colors", function() {
+    var red   = converter.hexToRgb("ff0000");
+    var green = converter.hexToRgb("00ff00");
+    var blue  = converter.hexToRgb("0000ff");
+
+    expect(red).to.deep.equal([255, 0, 0]);
+    expect(green).to.deep.equal([0, 255, 0]);
+    expect(blue).to.deep.equal([0, 0, 255]);
+  });
+});
+
+});
+
+const assert = require('assert');
+const nacl = require('tweetnacl');
+const bs58 = require('bs58');
+const encodeToBase58 = require('../lib/utils').encodeToBase58;
+const stringToArray = require('../lib/utils').stringToArray;
+
+
+function generateRequestFromBody(body) {
+  const keypair = nacl.sign.keyPair();
+  const signature = nacl.sign.detached(stringToArray(body), keypair.secretKey);
+
+  return {
+      headers: {
+          'x-fbtrex-userid': 31337, // eh eh eh
+          'x-fbtrex-publickey': encodeToBase58(keypair.publicKey),
+          'x-fbtrex-signature': encodeToBase58(signature)
+      },
+
+      body: body
+  };
+}
+
+describe('Signature validation', function () {
+  it('accepts a valid payloads signature', function () {
+      const verifyRequestSignature = require('../lib/utils').verifyRequestSignature;
+      const req = generateRequestFromBody('hurr durr hello internets');
+
+      assert.equal(verifyRequestSignature(req), true);
+  });
+
+  it('rejects invalid payload signature', function () {
+      const verifyRequestSignature = require('../lib/utils').verifyRequestSignature;
+      const req = generateRequestFromBody('hurr durr hello internets');
+      req.body = 'hurr durr hello internet'; // removing the last 's'
+
+      assert.equal(verifyRequestSignature(req), false);
+  });
+});
