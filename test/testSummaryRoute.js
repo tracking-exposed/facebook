9ef7a1329e3ba675e542d52a7ad8d045864674bd
Claudio Agosti <claudio@tracking.exposed>
2019-02-28 20:21:21 +0100

    many progress in testSummaryRoute

--- test/testSummaryRoute.js
+++ test/testSummaryRoute.js
@@ -8,30 +8,47 @@ const debug = require("debug")("test:testSummaryRoute");
 const mongo = require("../lib/mongo");
 const fixtures = require('../lib/fixtures');
 const adopters = require('../lib/adopters');
+const summary = require('../routes/summary');
 
 nconf.argv().env().file({ file: "config/unitTest.json" });
 
-const minimum = nconf.get('minimum') ? _.parseInt(nconf.get('minimum')) : 15;
+const minimum = nconf.get('minimum') ? _.parseInt(nconf.get('minimum')) : 1;
 const mandatory = ['timelines', 'impressions', 'metadata',
                    'htmls', 'summary', 'supporters' ];
 
+let dummyAccessToken = null;
+
 /* This first check the capacity of load data and verify they are avail */
-describe(`Checking data in ${nconf.get('mongodb')}`, function() {
+describe(`Routes pre-check about ${nconf.get('mongodb')}`, function() {
 
   function testExistence(columnName) {
     it(`Counting data in ${columnName} [${minimum} as \`minimum\`]`, function() {
-      return mongo
-        .count( _.get(nconf.get('schema'), columnName), {})
-        .then(function(amount) {
-          expect(amount).to.be.above(minimum);
-        });
+    return mongo
+      .count( _.get(nconf.get('schema'), columnName), {})
+      .then(function(amount) {
+        expect(amount).to.be.above(minimum);
+      });
     });
   }
 
-  if("check the amount of data in the mandatory collections", function() {
+  it("check the amount of object in the mandatory collections", function() {
     return Promise.map(mandatory, function(c) {
       return testExistence(c);
     }, { concurrency: 1 });
+  });
+
+
+  it("fetch the dummy access token", function() {
+    return mongo
+      .read(nconf.get('schema').supporters, { userId: fixtures.mockUserId })
+      .tap(function(results) {
+        expect(results).to.have.lengthOf(1);
+      })
+      .then(_.first)
+      .tap(function(supporter) {
+        expect(supporter.userSecret).to.have.lengthOf(40);
+        dummyAccessToken = supporter.userSecret;
+      });
   });
 
 });
@@ -44,16 +61,27 @@ describe("Test 'data' route", function() {
 
   it("Check mockUpToken work as access token", function() {
     return adopters
-      .validateToken(fixtures.mockUserToken)
-      .then(function(supporter) {
+      .validateToken(dummyAccessToken)
+      .tap(function(supporter) {
         expect(supporter.pseudo).is.equal(fixtures.mockExpectedPseudo);
-        expect(supporter.userToken).is.equal(fixtures.mockUserToken);
       });
   });
 
+  it("retrieve summary as JSON", function() {
+    const fixedMax = 20;
+    return summary
+      .data({ params: { amount: fixedMax, userToken: dummyAccessToken }})
+      .then(function(answer) {
+        expect(answer).to.have.all.keys(['json']);
+        return answer.json;
+      })
+      .tap(function(content) {
+        expect(content).to.have.lengthOf.at.most(fixedMax);
+        expect(content).to.have.lengthOf.at.least(1);
+      });
+  });
 
 });
-
 
 describe("Test 'csv' route", function() {
 });
@@ -61,5 +89,3 @@ describe("Test 'extended' route", function() {
 });
 describe("Test 'semantics' route", function() {
 });
-
-
