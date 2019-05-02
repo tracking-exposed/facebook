3944ae1f81f83336d54f20070fcb75f8eb3fc0d5
Claudio Agosti <claudio@tracking.exposed>
2019-05-02 19:58:24 +0200

    implemented langInfo (and cache) #128

--- routes/semantics.js
+++ routes/semantics.js
@@ -152,16 +152,7 @@ function loud(req) {
     if(!validLanguage(req.params.lang))
         return { json: LanguageError };
 
-    return mongo
-        .aggregate(nconf.get('schema').semantics, [
-            { $match: { lang: req.params.lang, when: { "$lt": new Date(backintime) }} },
-            { $limit: MAXENTRIES },
-            { $group: { _id: '$title', wp: { $first: '$wp'}, count: { $sum: 1 }}},
-            { $sort: { "count": -1 }},
-            { $skip: skip },
-            { $limit: amount },
-            { $project: { "label": "$_id", _id: false, wp: true, count: true }}
-        ])
+    return loudKeywordsPipeline(MAXENTRIES, backintime, amount, skip, req.params.lang)
         .then(function(loudness) {
             debug("return loudness: %j", _.map(loudness, 'label'));
             return { json: loudness };
@@ -169,11 +160,110 @@ function loud(req) {
 
 };
 
+function loudKeywordsPipeline(maxentries, backintime, amount, skip, lang) {
+    return mongo
+        .aggregate(nconf.get('schema').semantics, [
+            { $match: { lang: lang, when: { "$lt": new Date(backintime) }} },
+            { $limit: maxentries },
+            { $group: { _id: '$title', wp: { $first: '$wp'}, count: { $sum: 1 }}},
+            { $sort: { "count": -1 }},
+            { $skip: skip },
+            { $limit: amount },
+            { $project: { "label": "$_id", _id: false, wp: true, count: true }}
+        ]);
+}
+
+const cache = _.reduce(supported, function(memo, l) {
+    _.set(memo, l, {
+        content: null,
+        computedAt: null,
+        next: null,
+    });
+    return memo;
+}, {} );
+
+function formatReturn(lang, updated) {
+    if(updated) {
+        cache[lang].content = updated.content;
+        cache[lang].computedAt = updated.computedAt;
+        cache[lang].next = updated.next
+    }
+    return {
+        json: {
+            content: cache[lang].content,
+            computedt: cache[lang].computedAt.toISOString(),
+            next: cache[lang].next.toISOString(),
+        }
+    };
+};
+
+
+function langinfo(req) {
+    /* this API returns an complete information on a language population, with number of 
+     * active contributors in the last 48 hours and total keyword seen in the last 48 hours,
+     * this API do not support paging and is meant to be cached */
+    const MAXENTRIES = 60000;
+    const DEFAULTAMOUNT = 10;
+    const hoursNumber = 48;
+    const backintime = moment().subtract(hoursNumber, 'h').toISOString();
+    const amount = 13;
+    const skip = 0;
+    const lang = req.params.lang;
+    const cacheMinutes = 60 * 2;
+
+    if(!validLanguage(lang))
+        return { json: LanguageError };
+
+    if(cache[lang].next && moment().isBefore(cache[lang].next) && cache[lang].content) {
+        debug("langInfo %s, using cached version", lang);
+        return formatReturn(lang);
+    }
+
+    debug("langInfo %s, retrieving from DB", lang);
+    return Promise.all([
+        loudKeywordsPipeline(MAXENTRIES, backintime, amount, skip, lang),
+        mongo.aggregate(nconf.get('schema').semantics, [
+            { $sort: { when: -1 }},
+            { $match: { lang: req.params.lang, when: { "$gt": new Date(backintime) } }},
+            { $limit: MAXENTRIES },
+            { $group: { _id: "$label" }},
+            { $group: { _id: null, amount: { "$sum": 1 } }},
+        ]).then(function(o) { return (o && o[0] && o[0].amount) ? obj[0].amount : 0 }),
+        mongo.aggregate(nconf.get('schema').labels, [
+            { $sort: { when: -1 }},
+            { $match: { lang: lang, when: { "$gt": new Date(backintime) } }},
+            { $limit: MAXENTRIES },
+            { $lookup: { from: 'summary', localField: 'semanticId', foreignField: 'semanticId', as: 'summary' }},
+            { $group: { _id: "dummy", profiles: { $addToSet: "$summary.user"} }}
+        ]).then(function(usermess) {
+            // remind/TODO grouped by size and anonymized, this can be a nice stats
+            // _.flatten(usermess[0].profiles)
+            if(!(usermess && usermess[0] && usermess[0].profiles))
+                return 0;
+            return _.size(_.uniq(_.flatten(usermess[0].profiles)))
+        })
+    ])
+    .then(function(results) {
+        let retval = {
+            consideredHoursWindow: hoursNumber,
+            language: req.params.lang,
+            most: _.map(results[0], 'label'),
+            labelsCount: results[1],
+            contributors: results[2],
+        };
+        return formatReturn(lang, {
+            content: retval,
+            computedAt: moment(),
+            next: moment().add(cacheMinutes, 'minutes')
+        });
+    });
+}
 
 module.exports = {
     labels,
     semantics,
     enrich,
     loud,
-    noogle
+    noogle,
+    langinfo
 };
