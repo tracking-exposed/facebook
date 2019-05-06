27aaa0339a342ee09affefdaea144335fdf97291
Claudio Agosti <claudio@tracking.exposed>
2019-05-06 13:14:19 +0200

    implemented two RSS mandatory API and new jquery custom support

--- bin/install-script.sh
+++ bin/install-script.sh
@@ -7,6 +7,7 @@ mkdir -p dist/css &&
 mkdir -p dist/js/local 
 mkdir -p dist/images 
 mkdir -p $V 
+mkdir -p rss/keywords
 
 echo "User-agent: *\nAllow: /\nDisallow: /personal/\n" >> dist/robots.txt
 
@@ -21,7 +22,6 @@ cp -r sections/webscripts/* dist/js/local/
 cp -r node_modules/bootstrap/dist/* dist/
 cp -r support/tampermonkey/autoscroll.user.js dist/
 
-cp node_modules/jquery/dist/jquery.js $V 
 cp node_modules/d3/d3.min.js $V 
 cp node_modules/c3/c3.min.js $V 
 cp node_modules/moment/min/moment.min.js $V 
@@ -38,4 +38,13 @@ cp node_modules/reveal.js/lib/font/league-gothic/* dist/css/
 
 cd $V
 curl -O -J -L https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js
-cd ../..
+cd ..
+
+curl -O -J -L https://github.com/tracking-exposed/binaries/raw/master/libs/jquery-ui-1.12.1.custom.zip
+unzip jquery-ui-1.12.1.custom.zip
+cd jquery-ui-1.12.1.custom
+mv external/jquery/jquery.js ../js
+mkdir ../js/jquery-ui
+mv jquery-*.* ../js/jquery-ui
+cd ..
+
