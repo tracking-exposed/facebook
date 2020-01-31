c120202f0b3b40ce0abda88ddbaa08ba1c8fffb6
Claudio <claudio@tracking.exposed>
2020-01-31 23:58:27 +0100

    count-o-clock v2

--- bin/count-o-clock.js
+++ bin/count-o-clock.js
@@ -2,7 +2,6 @@
 const _ = require('lodash');
 const moment = require('moment');
 const statsd = require('debug')('bin:count-o-clock:stats');
-const tmlnd = require('debug')('bin:count-o-clock:tmln');
 const debug = require('debug')('bin:count-o-clock');
 const nconf= require('nconf');
 
@@ -23,21 +22,13 @@ async function computeCount(mongoc, statinfo, filter) {
      * this function is called twice: once with 'hour' filter
      * and another with 'day' */
 
-    /* debug("HourCollection: [%s], named %s, has %d variables",
-        statinfo.column, statinfo.name, _.size(statinfo.variables)); */
-
-    let counting = await _.map(statinfo.variables, async function(v) {
-        const thisfilter = Object.assign({}, filter, v.selector);
-        const amount = await mongo.count(mongoc, statinfo.column, thisfilter);
-        return _.set({}, v.name, amount);
-    });
-
-    /* todo other kind of calculus which are not count */
-    return await Promise
-        .all(counting)
-        .catch(function(error) {
-            debug("Error in computeHourCount (%s): %s", statinfo.name, error.message);
-        });
+    const counts = {};
+    for (v of statinfo.variables) {
+        let thisfilter = Object.assign({}, filter, v.selector);
+        let amount = await mongo.count(mongoc, statinfo.column, thisfilter);
+        _.set(counts, v.name, amount);
+    }
+    return counts;
 };
 
 async function trexstats() {
@@ -50,24 +41,29 @@ async function trexstats() {
     statsd("Loaded %d possible statistics%s: %d to be done",
         _.size(statsMap), name ? `, demanded '${name}'` : "", _.size(tobedone));
 
-    let statsp = await _.map(tobedone, async function(statinfo) {
+    /* Hours count */
+    for (statinfo of tobedone) {
         const hoursref = aggregated.hourData(statshour);
         const hourfilter = _.set({}, statinfo.timevar, {
             $gte: new Date(hoursref.reference),
             $lt: new Date(hoursref.hourOnext)
         });
         const hourC = await computeCount(mongoc, statinfo, hourfilter);
+
         statsd("Hour computed %s: %j", statinfo.name, hourC);
-        const entry = _.reduce(hourC, function(memo, e) {
-            return _.merge(memo, e);
+        const entry = _.reduce(hourC, function(memo, amount, name) {
+            _.set(memo, name, amount);
+            return memo;
         }, {
             hourId: hoursref.hourId,
             hour: new Date(hoursref.hourOnly),
             name: statinfo.name
         });
-        const rv1 = await mongo.upsertOne(mongoc, schema.stats, { hourId: hoursref.hourId, name: statinfo.name }, entry);
+        await mongo.upsertOne(mongoc, schema.stats, { hourId: hoursref.hourId, name: statinfo.name }, entry);
+    }
 
-        /* -- Day -- */
+    /* -- Day count -- */
+    for (statinfo of tobedone) {
         const dayref = aggregated.dayData(statshour);
         const dayfilter = _.set({}, statinfo.timevar, {
             $gte: new Date(dayref.reference),
@@ -75,24 +71,18 @@ async function trexstats() {
         });
         const dayC = await computeCount(mongoc, statinfo, dayfilter);
         statsd("Day computed %s: %j", statinfo.name, dayC);
-        const ready = _.reduce(dayC, function(memo, e) {
-            return _.merge(memo, e);
+        const ready = _.reduce(dayC, function(memo, amount, name) {
+            _.set(memo, name, amount);
+            return memo;
         }, {
             dayId: dayref.dayId,
             day: new Date(dayref.dayOnly),
             name: statinfo.name
         });
-        const rv2 = await mongo.upsertOne(mongoc, schema.stats, { dayId: dayref.dayId, name: statinfo.name }, ready);
-
-    });
-
-    const updates = await Promise.all(statsp)
-        .catch(function(error) {
-            debug("Error in main function: %s", error.message);
-        });
+        await mongo.upsertOne(mongoc, schema.stats, { dayId: dayref.dayId, name: statinfo.name }, ready);
+    };
 
     await mongoc.close();
-    return updates;
 };
 
 /* counter of time expent on facebook. 
@@ -109,121 +99,10 @@ function estimateDuration(impressions) {
     ).asSeconds();
 };
 
-async function dailyTimelines() {
-    const hoursago = utils.parseIntNconf('hoursago', 0);
-    const statshour = moment().subtract(hoursago, 'h').format();
-    const mongoc = await mongo.clientConnect();
-    const dayref = aggregated.dayData(statshour);
-    const timeFilter = {
-        startTime: {
-            $gte: new Date(dayref.reference),
-            $lt: new Date(dayref.dayOnext)
-        }
-    };
-
-    const userPerDay = await mongo.distinct(
-        mongoc,
-        schema.timelines,
-        "userId",
-        timeFilter );
-
-    tmlnd("Users active in [%j] = %d", timeFilter, _.size(userPerDay));
-
-    const updates = await _.reduce(userPerDay, async function(memo, userId) {
-
-        const filter = _.assign(timeFilter, {"userId": userId});
-        const mr = await mongo.aggregate(mongoc, schema.timelines, [
-            { $match: filter },
-            { $group: { _id: {
-                    year:  { $year: "$startTime" },
-                    month: { $month: "$startTime" },
-                    day:   { $dayOfMonth: "$startTime" },
-                    userId: "$userId"
-                },
-                day: { $first: "$startTime" },
-                ids: { $addToSet: "$id" },
-            }},
-            { $project: { userId: "$_id.userId", count: "$_id.count", day: true, "timelineId": "$ids", _id: false }},
-            { $sort: { day: -1 }},
-            { $unwind: "$timelineId" },
-            { $lookup: { from: 'metadata', localField: 'timelineId', foreignField: 'timelineId', as: 'metadata'}},
-            { $lookup: { from: 'impressions2', localField: 'timelineId', foreignField: 'timelineId', as: 'impressions'}}
-        ]);
-
-        const extended = _.map(mr, function(timeline) {
-            timeline.dayString = moment(timeline.day).format("YYYY-MM-DD");
-            timeline.durationSeconds = estimateDuration(timeline.impressions);
-            return timeline;
-        })
-
-        const grouped = _.values(_.groupBy(extended, 'dayString'));
-
-        tmlnd("aggregate answers from %d, %d entries, aggregated in %d days",
-            userId, _.size(mr), _.size(grouped) );
-
-        const aggregated = _.map(grouped, function(dayr) {
-
-            // TODO refactor
-            // save in evests already the pseudoaninymized timeline name 
-            // TODO THEN -- query summary this would make simpler the sums below.
-
-            const metadatas = _.flatten(_.map(dayr, 'metadata'));
-            const ntimelines = _.size(dayr);
-            const npost = _.size(metadatas);
-            const nature = _.countBy(metadatas, 'nature');
-            const totalSeconds = _.sum(_.map(dayr, 'durationSeconds'));
-            const duration = moment.duration({ seconds: totalSeconds }).humanize();
-            const sources = _.size(_.uniq(_.map(metadatas, function(m) {
-                return m.attributions[0].content;
-            })));
-
-            return {
-                day: dayr[0].dayString,
-                dayTime: new Date(dayr[0].dayString),
-                id: utils.hash({
-                    dayString: dayr[0].dayString,
-                    userId: userId,
-                    frequency: 'daily'
-                }),
-                ntimelines,
-                npost,
-                nature,
-                totalSeconds,
-                duration,
-                sources,
-                userId: userId,
-            };
-        });
-
-        if(_.size(aggregated) != 1)
-            debug("Unexpected behavior, only one day should be catch here (%d)", _.size(aggregated));
-
-        try {
-            let r = await mongo.upsertOne(
-                mongoc, schema.tmlnstats, { id: aggregated[0].id }, aggregated[0] );
-            return _.concat(memo, r);
-        } catch(error) {
-            tmlnd("upsertOne error: %d - %s", aggregated[0].userId, error.message);
-            return _.concat(memo, null);
-        }
-
-    }, [] );
-
-    return Promise.all(updates)
-        .then(function() {
-            mongoc.close();
-            return updates;
-        });
-}
-
-
 async function execute() {
     try {
         debug("Starting execution with %d hoursago", utils.parseIntNconf('hoursago', 0));
-        let res1 = await dailyTimelines();
         let res2 = await trexstats();
-        if(!res1 || !res2)
-            debug("Error?");
     } catch(error) {
         debug("Unexpected error: %s", error.message);
     } finally {
