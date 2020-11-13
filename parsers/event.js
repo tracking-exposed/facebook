b665b279286f3fa4a023b59e432c9566e3a326a6
Claudio <claudio@tracking.exposed>
2020-11-13 01:10:17 +0100

    removed dead code

--- parsers/event.js
+++ parsers/event.js
@@ -1,27 +1,9 @@
 var _ = require('lodash');
 var debug = require('debug')('parsers:components:event');
 
-function event(envelop) {
+function event(envelop, previous) {
 
-    /* it return if a special event which has not been spot as link, is actually the permaLink
-     * a7f651c0b23396dd91cc06d478eb25908b1b9b23 */
-
-    let exist = _.filter(envelop.jsdom.querySelectorAll('[href^="/events/"]'), function(n) {
-        return n.hasAttribute('data-hovercard')
-    });
-
-    if(!_.size(exist))
-        return null;
-    if(_.size(exist) > 1)
-        debug("Whoa, very atypical! %j", _.map(exist, '.outerHTML'));
-
-    exist = _.first(exist);
-
-    debug("Found event: %s", exist.getAttribute('href').replace(/\?.*/, ''));
-    return {
-        permaLink: exist.getAttribute('href').replace(/\?.*/, ''),
-        fblinktype: 'events'
-    };
+    return null;
 };
 
 module.exports = event;
--- parsers/sequence.js
+++ parsers/sequence.js
@@ -34,51 +34,5 @@ module.exports = {
     external: require('./external'),
     event: require('./event'),
     // regexp: require('../regexp'),
-    opengraph: require('./opengraph'),
     video: require('./video'),
 };
-
-/*
-const pipeline = require('./index');
-const findOutNature = require('../nature');
-const semantic = require('../semantic');
-function mandatoryPostId(envelop) {
-    var p = _.get(envelop, 'feed_id.postId');
-    if(!p)
-        p = _.get(envelop, 'linkontime.postId');
-    return p;
-};
-function sequence(envelop) {
-    debug("🡆🡆  http://localhost:1313/debug/html/#%s", envelop.impression.id);
-    ];
-    envelop.errors = [];
-    envelop = _.reduce(flist, function(memo, fname) {
-        try {
-            let result = pipeline[fname](memo);
-            if(result)
-                _.set(memo, fname, result);
-            return memo;
-        } catch(error) {
-            debug("at %s: error catch <%s>", fname, error.message);
-            debug(error.stack);
-            memo.errors.push({
-                step: fname,
-                error: error.message            
-            });
-        } finally {
-            return memo;
-        }
-    }, envelop);
-
-    envelop.postId = mandatoryPostId(envelop); // mandatory for the postIdCount
-    envelop.fullText = semantic.fullText(envelop); // for the semanticCount & semanticId
-    envelop.fullTextSize = _.size(envelop.fullText);
-    envelop.nature = findOutNature(envelop);
-
-    /* this returns only the metadata. possible extension of the metadata
-     * structure it is possibile, or possible recomputation of specific
-     * components too. Just at the moment the mongo functions calling this don't even
-     * support or care of the possibility 
-    return envelop;
-};
-*/
