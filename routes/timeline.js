e620a92981f41c024c693f2aabbb0c3a237e789c
Claudio <claudio@syrianarchive.org>
2021-09-20 18:55:58 +0200

    completed #169

--- routes/timeline.js
+++ routes/timeline.js
@@ -34,31 +34,44 @@ async function impressionStatsLegacy(req) {
     debugger;
 }
 async function impressionStats(req) {
-    debug(req.params.day);
-    const xx = req.params.day;
-    const when = moment(xx);
+    const dayString = req.params.day;
+    if(!dayString || !dayString.match(/(\d+)-(\d+)-(\d+)/))
+        throw new Error("Invalid date format")
+    const when = moment(dayString);
     const startOf = new Date(when.startOf('day').toISOString());
     const endOf = new Date(when.add(1, 'd').startOf('day').toISOString());
 
     if(!startOf || !endOf)
         throw new Error("Errorfull date parsing?");
 
-    const impresslists = await mongo.read(nconf.get('schema').impressions,
-        { impressionTime: { $lt: endOf, $gt: startOf } }
-    );
-    /* an impression object has these fields: {
-        "_id" : ObjectId("5fe1cfb7e0fc454e9c842945"),
-        "visibility" : "private",
-        "from" : "standard",
+    const enhancedimpr = await mongo.aggregate(
+        nconf.get('schema').impressions, [
+            { "$match": { impressionTime: { $lt: endOf, $gt: startOf }}},
+            { "$lookup": { from: 'htmls2', localField: 'htmlId', foreignField: 'id', as: 'hinfo' }},
+            { "$lookup": { from: 'metadata2', localField: 'htmlId', foreignField: 'id', as: 'metadata' }}
+        ]);
+    /* an enhanced impression object has these fields: {
+        "visibility" : "public",
         "kind" : "post",
-        "timelineId" : "325b979b77444fa341079cc2d7dbe4a6351ba522",
-        "id" : "b0cf934ff7da4898af5139fccbe9822cc765ea62",
+        "timelineId" : "153062305c6273641a5b3c57b790671fd38e7f7a",
+        "id" : "b0cf934f33da4898af5139fccbe9822cc765ea62",
         "userId" : 3217423097589078284827374297823,
         "impressionOrder" : 19,
-        "impressionTime" : ISODate("2020-12-22T10:51:35.686Z")
-    } */
-
-    const grouped = _.groupBy(impresslists, 'timelineId');
+        "impressionTime" : ISODate("2020-12-20T21:25:12.542Z"),
+        "htmlId" : "5e26ae0ca1dd93dfb33c316a390d705d58f34e9d",
+        "metadata" : [ {
+            "texts" : [ 
+                "Pensa a un’auto elettrica che ti permetta di ritrovare ciò che rende "
+            ],
+            "nature" : {
+                "kind" : "ad",
+                "from" : "standard",
+                "visibility" : "public"
+            },
+            "pseudo" : "okra-ravioli-tapioca",
+        }
+    ]}  */
+    const grouped = _.groupBy(enhancedimpr, 'timelineId');
     /* this aggregate in an object for every timelineId */
     const final = _.reduce(grouped, function(memo, impressions, timelineId) {
         const x = {};
@@ -71,34 +84,46 @@ async function impressionStats(req) {
         x.info = [];
         _.each(ordered, function(impression) {
             /* here copy visibility|kind into a list ordered by impression order */
+            const nature = _.get(_.first(_.get(impression, 'metadata', [])), 'nature');
             const info = (impression.visibility === 'private') ? 'private' :
-                (impression.kind === 'post' ? 'organic' : impression.kind); // 'ad'
+                nature ? nature.kind : impression.kind;
             /* debug("copying %s Order %d array position %d",
                 info, impression.impressionOrder, _.size(x.info) ); */
             x.info.push(info);
             x.sessionTimings.push(impression.impressionTime);
         });
-        /* x is:
-            sessionId	"2b87726ae4a2d5fd316da929da1a5e3dd0a33681"
+        /* 'x' is an object for every session, the collection
+               contains all the evidence per day requested.
+
+            sessionId "3f78aa4e03788fcb0d425842572eefbf2c002327"
             sessionTimings
-            0	"2020-12-23T14:34:52.965Z"
-            1	"2020-12-23T14:34:52.966Z"
-            2	"2020-12-23T14:34:54.006Z"
-            3	"2020-12-23T14:34:54.006Z"
-            4	"2020-12-23T14:34:54.007Z"
-            5	"2020-12-23T14:34:54.007Z"
-            6	"2020-12-23T14:34:54.008Z"
+                0	"2020-12-20T18:27:59.123Z"
+                1	"2020-12-20T18:27:59.136Z"
+                2	"2020-12-20T18:28:05.402Z"
+                3	"2020-12-20T18:28:05.462Z"
+                4	"2020-12-20T18:28:05.467Z"
+                5	"2020-12-20T18:28:05.478Z"
+                6	"2020-12-20T18:28:05.508Z"
+                7	"2020-12-20T18:28:11.030Z"
+                8	"2020-12-20T18:28:11.095Z"
+                9	"2020-12-20T18:28:11.149Z"
+                10	"2020-12-20T18:28:11.198Z"
             info
-            0	"organic"
-            1	"organic"
-            2	"private"
-            3	"organic"
-            4	"organic"
-            5	"private"
-            6	"organic"            */
+                0	"post"
+                1	"ad"
+                2	"post"
+                3	"post"
+                4	"post"
+                5	"private"
+                6	"ad"
+                7	"post"
+                8	"post"
+                9	"post"
+                10	"private"                */
         memo.push(x);
         return memo;
     }, []);
+    debug("Producing %d %d collected session", dayString, _.size(final));
     return { json: final };
 }
 
